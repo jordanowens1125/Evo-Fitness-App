386ce6358ac6624165772dd4200e3030068ec155
Jordan Owens <jordanowens1125@gmail.com>
2024-06-02 14:40:12 -0500

    before window unload log out

--- client/src/hooks/useLogOut.js
+++ client/src/hooks/useLogOut.js
@@ -2,11 +2,13 @@ import useAuthContext from "./useAuthContext";
 
 export const useLogOut = () => {
   const { dispatch } = useAuthContext();
-
   const signOut = () => {
     //remove user from storage
     localStorage.removeItem("user");
     dispatch({ type: "LOGOUT" });
   };
+  window.addEventListener("beforeunload", () => {
+    signOut()
+  });
   return { signOut };
 };
